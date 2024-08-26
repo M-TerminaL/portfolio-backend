95636c5154c822db57962167986d1e2664346398
Alireza Babaei <andromedaa5692@gmail.com>
2024-08-26 20:42:32 +0330

    config meta description and alt for ceo

--- home_module/models.py
+++ home_module/models.py
@@ -13,6 +13,14 @@ class ProfileSetting(models.Model):
     instagram = models.CharField(max_length=200, verbose_name='آی دی اینستاگرام')
     telegram = models.CharField(max_length=200, verbose_name='آی دی تلگرام')
     whatsapp = models.CharField(max_length=200, verbose_name='شماره واتس آپ')
+    meta_description = models.CharField(max_length=160, verbose_name='متاتگ برای سئو', null=True, blank=True,
+                                        help_text='برای افزایش سئو یک متن 160 کاراکتری ، ترغیب کننده ، جذاب و مرتبط با محتوای قرار گرفته در صفحه اصلی سایت بنویسید.')
+    keyword_one = models.CharField(max_length=160, verbose_name='کلمه کلیدی اول', null=True, blank=True,
+                                   help_text='یک کلمه کلیدی برای افزایش رتبه گوگل یا سئو ، متناسب با محتوای این صفحه بنویسید')
+    keyword_two = models.CharField(max_length=160, verbose_name='کلمه کلیدی دوم', null=True, blank=True,
+                                   help_text='یک کلمه کلیدی برای افزایش رتبه گوگل یا سئو ، متناسب با محتوای این صفحه بنویسید')
+    keyword_three = models.CharField(max_length=160, verbose_name='کلمه کلیدی سوم', null=True, blank=True,
+                                     help_text='یک کلمه کلیدی برای افزایش رتبه گوگل یا سئو ، متناسب با محتوای این صفحه بنویسید')
     is_main_profile = models.BooleanField(default=False, verbose_name='پروفایل اصلی',
                                           help_text='فقط یک پروفایل مجاز به استفاده می باشد', db_index=True)
 
