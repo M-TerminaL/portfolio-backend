81bb932ea5b245afc664146d4b021ce066808ad8
Alireza <andromedaa5692@gmail.com>
2025-03-05 23:42:12 +0330

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&emoulc)j)mrml#^npcys*_p58o-1a64x=3l!pn*35shnogarc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
